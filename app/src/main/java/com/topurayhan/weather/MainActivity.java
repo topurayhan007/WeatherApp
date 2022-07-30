1269084774857d616b12a1d8ca7a90e85d758258
topurayhan007 <topurayhan007@gmail.com>
2022-07-30 12:31:13 +0600

    Added everthing done till now

---
+++ app/src/main/java/com/topurayhan/weather/MainActivity.java
@@ -0,0 +1,125 @@
+package com.topurayhan.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    @SuppressLint("StaticFieldLeak")
+    static TextView location;
+    @SuppressLint("StaticFieldLeak")
+    static TextView description;
+    @SuppressLint("StaticFieldLeak")
+    static TextView humidity;
+    @SuppressLint("StaticFieldLeak")
+    static TextView pressure;
+    @SuppressLint("StaticFieldLeak")
+    static ImageView icon;
+    @SuppressLint("StaticFieldLeak")
+    static TextView mainTemp;
+    @SuppressLint("StaticFieldLeak")
+    static EditText search;
+    @SuppressLint("StaticFieldLeak")
+    static TextView windSpeed;
+    @SuppressLint("StaticFieldLeak")
+    static TextView visibility;
+    @SuppressLint("StaticFieldLeak")
+    static TextView hour1temp, hour2temp, hour3temp, hour4temp, hour5temp, hour1, hour2, hour3, hour4, hour5,
+            day1temp, day2temp, day3temp, day4temp, day1, day2, day3, day4;
+    @SuppressLint("StaticFieldLeak")
+    static ImageView hour1img, hour2img, hour3img, hour4img, hour5img, day1img, day2img, day3img, day4img;
+
+    static String cityName = "Dhaka";
+    static String key = "488f4111e6b7924073ff22cd896b2e2a";
+    static String iconLink = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        search = (EditText) findViewById(R.id.search);
+        location = (TextView) findViewById(R.id.city);
+        description = (TextView) findViewById(R.id.description);
+        mainTemp = (TextView) findViewById(R.id.tempMain);
+        humidity = (TextView) findViewById(R.id.humidity);
+        pressure = (TextView) findViewById(R.id.pressure);
+        windSpeed = (TextView) findViewById(R.id.windSpeed);
+        visibility = (TextView) findViewById(R.id.visibility);
+
+        hour1 = (TextView) findViewById(R.id.hour1);
+        hour2 = (TextView) findViewById(R.id.hour2);
+        hour3 = (TextView) findViewById(R.id.hour3);
+        hour4 = (TextView) findViewById(R.id.hour4);
+        hour5 = (TextView) findViewById(R.id.hour5);
+
+        hour1temp = (TextView) findViewById(R.id.hour1temp);
+        hour2temp = (TextView) findViewById(R.id.hour2temp);
+        hour3temp = (TextView) findViewById(R.id.hour3temp);
+        hour4temp = (TextView) findViewById(R.id.hour4temp);
+        hour5temp = (TextView) findViewById(R.id.hour5temp);
+
+        hour1img = (ImageView) findViewById(R.id.hour1img);
+        hour2img = (ImageView) findViewById(R.id.hour2img);
+        hour3img = (ImageView) findViewById(R.id.hour3img);
+        hour4img = (ImageView) findViewById(R.id.hour4img);
+        hour5img = (ImageView) findViewById(R.id.hour5img);
+
+        day1 = (TextView) findViewById(R.id.day1);
+        day2 = (TextView) findViewById(R.id.day2);
+        day3 = (TextView) findViewById(R.id.day3);
+        day4 = (TextView) findViewById(R.id.day4);
+
+        day1temp = (TextView) findViewById(R.id.day1temp);
+        day2temp = (TextView) findViewById(R.id.day2temp);
+        day3temp = (TextView) findViewById(R.id.day3temp);
+        day4temp = (TextView) findViewById(R.id.day4temp);
+
+        day1img = (ImageView) findViewById(R.id.day1img);
+        day2img = (ImageView) findViewById(R.id.day2img);
+        day3img = (ImageView) findViewById(R.id.day3img);
+        day4img = (ImageView) findViewById(R.id.day4img);
+
+        search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                boolean enter = false;
+                if (i == EditorInfo.IME_ACTION_DONE){
+                    cityName = search.getText().toString();
+                    search.clearFocus();
+                    if (checkWhiteSpace(cityName))
+                        cityName = cityName.substring(0, cityName.length() - 1);
+                    getWeather(cityName, key);
+                }
+                return false;
+            }
+
+
+        });
+
+        getWeather(cityName, key);
+
+
+    }
+
+    private void getWeather(String cityName, String key) {
+        Weather getData = new Weather();
+        getData.execute("https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+key+"&units=metric");
+    }
+
+    public boolean checkWhiteSpace(String cityName) {
+        String str = cityName;
+        if (str.charAt(str.length()-1) == ' '){
+            return true;
+        }
+        else
+            return false;
+    }
+}
---
+++ app/src/main/java/com/topurayhan/weather/Weather.java
@@ -0,0 +1,331 @@
+package com.topurayhan.weather;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.IonContext;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class Weather extends AsyncTask<String, Void, String> {
+    String result;
+    @Override
+    protected String doInBackground(String... urls) {
+        result = "";
+        URL link;
+        HttpURLConnection myConnection = null;
+
+        try{
+            link = new URL(urls[0]);
+            myConnection = (HttpURLConnection)link.openConnection();
+            InputStream in = myConnection.getInputStream();
+            InputStreamReader myStreamReader = new InputStreamReader(in);
+            int data = myStreamReader.read();
+            while (data != -1){
+                char current = (char)data;
+                result += current;
+                data = myStreamReader.read();
+            }
+            return result;
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        try{
+            // JSON Result stored in myObject
+            JSONObject myObject = new JSONObject(result);
+            Log.d("myObject", myObject.toString());
+
+            // City name and Country from JSON myObject
+            JSONObject city = new JSONObject(myObject.getString("city"));
+            String placeName = city.getString("name").toString();
+            Log.d("placeName", placeName.toString());
+            String country = city.getString("country").toString();
+            Log.d("country", country.toString());
+            String location = placeName+", "+country;
+
+            // Accessing the "list" Array Object
+            JSONArray list = myObject.getJSONArray("list");
+            Log.d("list", list.toString());
+
+            // Accessing the 1st Object from the Array Object "list"
+            JSONObject list0 = list.getJSONObject(0);
+            Log.d("list0", list0.toString());
+            JSONObject list1 = list.getJSONObject(1);
+            JSONObject list2 = list.getJSONObject(2);
+            JSONObject list3 = list.getJSONObject(3);
+            JSONObject list4 = list.getJSONObject(4);
+
+            // Accessing the "weather" from an object of Array Object "list"
+            JSONArray weather = list0.getJSONArray("weather");
+            JSONArray weather1 = list1.getJSONArray("weather");
+            JSONArray weather2 = list2.getJSONArray("weather");
+            JSONArray weather3 = list3.getJSONArray("weather");
+            JSONArray weather4 = list4.getJSONArray("weather");
+
+            Log.d("weather", weather.toString());
+
+            // "description" and "icon" from "weather"
+            String desc = weather.getJSONObject(0).getString("description").toString();
+            desc = desc.substring(0, 1).toUpperCase() + desc.substring(1);
+            Log.d("desc", desc.toString());
+
+
+            // Accessing the "main" from an object of Array Object "list"
+            JSONObject main = list0.getJSONObject("main");
+            Log.d("main", main.toString());
+            JSONObject main1 = list1.getJSONObject("main");
+            JSONObject main2 = list2.getJSONObject("main");
+            JSONObject main3 = list3.getJSONObject("main");
+            JSONObject main4 = list4.getJSONObject("main");
+
+            String time = list0.getString("dt_txt").toString();
+            Log.d("time", time.toString());
+            String[] split = time.split(" ");
+            String[] str = split[1].split(":");
+            Log.d("s1", str[0]);
+            int hr = Integer.parseInt(str[0]);
+            String hour1 = "";
+            if (hr > 12){
+                int temp = hr-12;
+                hour1 = String.valueOf(temp) + " PM";
+
+            }
+            else if (hr == 12){
+                hour1 = String.valueOf(hr) + " PM";
+            }
+            else if (hr == 0){
+                hour1 = String.valueOf(12) + " AM";
+            }
+            else{
+                hour1 = String.valueOf(hr) + " AM";
+            }
+            MainActivity.hour1.setText(hour1);
+
+            String time1 = list1.getString("dt_txt").toString();
+            String[] split2 = time1.split(" ");
+            String[] str2 = split2[1].split(":");
+            Log.d("s2", str2[0]);
+            int hr2 = Integer.parseInt(str2[0]);
+            String hour2 = "";
+            if (hr2 > 12){
+                int temp2 = hr2-12;
+                hour2 = String.valueOf(temp2) + " PM";
+
+            }
+            else if (hr2 == 12){
+                hour2 = String.valueOf(hr2) + " PM";
+            }
+            else if (hr2 == 0){
+                hour2 = String.valueOf(12) + " AM";
+            }
+            else{
+                hour2 = String.valueOf(hr2) + " AM";
+            }
+            MainActivity.hour2.setText(hour2);
+
+            String time2 = list2.getString("dt_txt").toString();
+            String[] split3 = time2.split(" ");
+            String[] str3 = split3[1].split(":");
+            Log.d("s3", str3[0]);
+            int hr3 = Integer.parseInt(str3[0]);
+            String hour3 = "";
+            if (hr3 > 12){
+                int temp3 = hr3-12;
+                hour3 = String.valueOf(temp3) + " PM";
+
+            }
+            else if (hr3 == 12){
+                hour3 = String.valueOf(hr3) + " PM";
+            }
+            else if (hr3 == 0){
+                hour3 = String.valueOf(12) + " AM";
+            }
+            else{
+                hour3 = String.valueOf(hr3) + " AM";
+            }
+            MainActivity.hour3.setText(hour3);
+
+            String time3 = list3.getString("dt_txt").toString();
+            String[] split4 = time3.split(" ");
+            String[] str4 = split4[1].split(":");
+            Log.d("s4", str4[0]);
+            int hr4 = Integer.parseInt(str4[0]);
+            String hour4 = "";
+            if (hr4 > 12){
+                int temp4 = hr4-12;
+                hour4 = String.valueOf(temp4) + " PM";
+
+            }
+            else if (hr4 == 12){
+                hour4 = String.valueOf(hr4) + " PM";
+            }
+            else if (hr4 == 0){
+                hour4 = String.valueOf(12) + " AM";
+            }
+            else{
+                hour4 = String.valueOf(hr4) + " AM";
+            }
+            MainActivity.hour4.setText(hour4);
+
+            String time4 = list4.getString("dt_txt").toString();
+            String[] split5 = time4.split(" ");
+            String[] str5 = split5[1].split(":");
+            Log.d("s5", str5[0]);
+            int hr5 = Integer.parseInt(str5[0]);
+            String hour5 = "";
+            if (hr5 > 12){
+                int temp5 = hr5-12;
+                hour5 = String.valueOf(temp5) + " PM";
+
+            }
+            else if (hr5 == 12){
+                hour5 = String.valueOf(hr5) + " PM";
+            }
+            else if (hr5 == 0){
+                hour5 = String.valueOf(12) + " AM";
+            }
+            else{
+                hour5 = String.valueOf(hr5) + " AM";
+            }
+            MainActivity.hour5.setText(hour5);
+
+
+            // "temp_max" & "temp_min" from "main"
+            double mainT = main.getDouble("temp");
+            Log.d("mainTemp", Double.toString(mainT));
+            int temp = (int) Math.round(mainT);
+            String mainTemp = String.valueOf(temp) ;
+            MainActivity.hour1temp.setText(mainTemp+ "°");
+            String icon = weather.getJSONObject(0).getString("icon").toString();
+            Log.d("icon", icon.toString());
+            // Setting the icon of the weather
+            Ion.with(MainActivity.hour1img)
+                    .load("http://openweathermap.org/img/wn/"+icon+"@2x.png");
+
+            double mainT1 = main1.getDouble("temp");
+            Log.d("mainTemp1", Double.toString(mainT1));
+            int temp1 = (int) Math.round(mainT1);
+            String mainTemp1 = String.valueOf(temp1) + "°";
+            MainActivity.hour2temp.setText(mainTemp1);
+            String icon1 = weather1.getJSONObject(0).getString("icon").toString();
+            Log.d("icon1", icon1.toString());
+            Ion.with(MainActivity.hour2img)
+                    .load("http://openweathermap.org/img/wn/"+icon1+"@2x.png");
+
+            double mainT2 = main2.getDouble("temp");
+            Log.d("mainTemp2", Double.toString(mainT2));
+            int temp2 = (int) Math.round(mainT2);
+            String mainTemp2 = String.valueOf(temp2) + "°";
+            MainActivity.hour3temp.setText(mainTemp2);
+            String icon2 = weather2.getJSONObject(0).getString("icon").toString();
+            Log.d("icon2", icon2.toString());
+            Ion.with(MainActivity.hour3img)
+                    .load("http://openweathermap.org/img/wn/"+icon2+"@2x.png");
+
+            double mainT3 = main3.getDouble("temp");
+            Log.d("mainTemp3", Double.toString(mainT3));
+            int temp3 = (int) Math.round(mainT3);
+            String mainTemp3 = String.valueOf(temp3) + "°";
+            MainActivity.hour4temp.setText(mainTemp3);
+            String icon3 = weather3.getJSONObject(0).getString("icon").toString();
+            Log.d("icon3", icon3.toString());
+            Ion.with(MainActivity.hour4img)
+                    .load("http://openweathermap.org/img/wn/"+icon3+"@2x.png");
+
+            double mainT4 = main4.getDouble("temp");
+            Log.d("mainTemp4", Double.toString(mainT4));
+            int temp4 = (int) Math.round(mainT4);
+            String mainTemp4 = String.valueOf(temp4) + "°";
+            MainActivity.hour5temp.setText(mainTemp4);
+            String icon4 = weather4.getJSONObject(0).getString("icon").toString();
+            Log.d("icon4", icon4.toString());
+            Ion.with(MainActivity.hour5img)
+                    .load("http://openweathermap.org/img/wn/"+icon4+"@2x.png");
+
+
+//
+//            double max = main.getDouble("temp_max");
+//            Log.d("maxTemp", Double.toString(max));
+//            int temp1 = (int) Math.round(max);
+//            String maxTemp = String.valueOf(temp1);
+//
+//            double min = main.getDouble("temp_min");
+//            Log.d("minTemp", Double.toString(max));
+//            int temp2 = (int) Math.round(max);
+//            String minTemp = String.valueOf(temp2);
+//            Log.d("minTemp", minTemp.toString());
+
+            String humidity = main.getString("humidity").toString() + " %";
+            Log.d("humidity", humidity.toString());
+
+            String pressure = main.getString("pressure").toString() + " Pa";
+            Log.d("pressure", pressure.toString());
+
+            JSONObject wind = list0.getJSONObject("wind");
+            double windTemp = wind.getDouble("speed");
+            windTemp = windTemp * 3.6;
+            final DecimalFormat df = new DecimalFormat("0.0");
+            String windSpeed = df.format(windTemp) + " km/h";
+            Log.d("windSpeed", windSpeed.toString());
+
+
+            int visible = list0.getInt("visibility");
+            int tempVisible = visible/1000;
+            String visibility = String.valueOf(tempVisible) + " km";
+            Log.d("visibility", visibility);
+
+
+
+            // Setting all info to views in MainActivity
+            MainActivity.location.setText(location);
+            MainActivity.mainTemp.setText(mainTemp);
+//            MainActivity.maxTemp.setText(maxTemp);
+//            MainActivity.minTemp.setText(minTemp);
+            MainActivity.description.setText(desc);
+            MainActivity.humidity.setText(humidity);
+            MainActivity.pressure.setText(pressure);
+            MainActivity.visibility.setText(visibility);
+            MainActivity.windSpeed.setText(windSpeed);
+
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
